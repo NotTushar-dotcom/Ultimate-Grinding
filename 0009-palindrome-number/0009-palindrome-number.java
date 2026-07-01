142498bb533d176bcde76b2ea21ad4fc6ee4a1ee
Tushar Goswami <tushh45@gmail.com>
2026-07-01 10:50:49 +0530

    Time: 5 ms (79.69%), Space: 46 MB (33.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x < 0) {
+            return false;
+        }
+        int org = x;
+        int rev = 0;
+
+        while(x != 0) {
+            int ld = x % 10;
+            x = x / 10;
+            rev = ( rev * 10 ) + ld;
+        }
+        return rev == org;
+    }
+}
